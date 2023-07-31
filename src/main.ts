d375275011b1a84fcdbf4fb17021c3e0dec54654
Josep Valls <alfredvays@gmail.com>
2023-07-31 21:15:56 +0200

    CORS in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as logger from 'morgan';
+import { CORS } from './constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,8 +11,12 @@ async function bootstrap() {
 
   const configService = app.get(ConfigService);
 
+  app.enableCors(CORS);
+
   app.setGlobalPrefix('api/v1');
 
   await app.listen(configService.get('PORT'));
+
+  console.log(`APP running on ${await app.getUrl()}`);
 }
 bootstrap();
