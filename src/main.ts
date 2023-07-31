124fc5a5050a02ac88be5995c58c2db5b3525b27
Josep Valls <alfredvays@gmail.com>
2023-07-31 20:34:40 +0200

    add logger in develop environment

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as logger from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(logger('dev'));
 
   const configService = app.get(ConfigService);
 
