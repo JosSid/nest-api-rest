067e5244f1fb1f4d9dc392bb5de4db19d365fe05
Josep Valls <alfredvays@gmail.com>
2023-08-02 21:28:57 +0200

    HandleErrors and usersController

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -1,12 +1,44 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { UsersService } from '../services/users.service';
+import { UserDTO, UserUpdateDTO } from '../dto/user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Get()
-  getHello() {
-    return this.usersService.findUsers();
+  @Get('all')
+  public async findAllUsers() {
+    return await this.usersService.findUsers();
+  }
+
+  @Get(':id')
+  public async findUserById(@Param('id') id: string) {
+    return await this.usersService.findUserById(id);
+  }
+
+  @Post('register')
+  public async registerUser(@Body() body: UserDTO) {
+    return await this.usersService.createUser(body);
+  }
+
+  @Put('edit/:id')
+  public async updateUser(
+    @Param('id') id: string,
+    @Body() body: UserUpdateDTO,
+  ) {
+    return await this.usersService.updateUser(body, id);
+  }
+
+  @Delete('delete/:id')
+  public async deleteUser(@Param('id') id: string) {
+    return await this.usersService.deleteUser(id);
   }
 }
